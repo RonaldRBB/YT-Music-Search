0aaf7ccfbdf1505d0025c2791e9b6ed45f9ec5d7
Ronald Bello <ronaldbello2@gmail.com>
2024-01-15 00:28:04 -0300

    ya guarda la lista

--- app/models/video_list.py
+++ app/models/video_list.py
@@ -1,9 +1,6 @@
 """Video List Model."""
-# table vide_list
-# id (primary key)
-# name (big string)
-# opened (true false)
-from sqlalchemy import Boolean, Text
+from datetime import datetime
+from sqlalchemy import Boolean, Text, DateTime
 from sqlalchemy.orm import Mapped, mapped_column
 
 from config import DB_PREFIX, Base
@@ -14,9 +11,22 @@ class VideoList(Base):
 
     __tablename__ = f"{DB_PREFIX}_video_list"
     id: Mapped[int] = mapped_column(primary_key=True)
+    _folder: Mapped[str] = mapped_column("folder", Text, nullable=False)
     _name: Mapped[str] = mapped_column(
         "name", Text, nullable=False, unique=True)
+    _created_at: Mapped[datetime] = mapped_column(
+        "created_at", DateTime, nullable=False, default=None)
     _opened: Mapped[bool] = mapped_column("opened", Boolean, default="false")
+
+    @property
+    def folder(self) -> str:
+        """Get folder."""
+        return self._folder
+
+    @folder.setter
+    def folder(self, value: str) -> None:
+        """Set folder."""
+        self._folder = value
 
     @property
     def name(self) -> str:
@@ -27,6 +37,16 @@ class VideoList(Base):
     def name(self, value: str) -> None:
         """Set name."""
         self._name = value
+
+    @property
+    def created_at(self) -> datetime:
+        """Get created_at."""
+        return self._created_at
+
+    @created_at.setter
+    def created_at(self, value: datetime) -> None:
+        """Set created_at."""
+        self._created_at = value
 
     @property
     def opened(self) -> bool:
---
+++ loaddb.py
@@ -0,0 +1,49 @@
+"""Script para buscar videos."""
+import os
+from datetime import datetime
+
+from sqlalchemy.exc import IntegrityError
+
+from app.models import VideoList
+from config import session
+
+
+def get_sorted_files(directory):
+    """Funcion get_sorted_files."""
+    file_paths = []
+    for root, _, files in os.walk(directory):
+        for file in files:
+            file_paths.append(os.path.join(root, file))
+    return sorted(file_paths, key=lambda f: os.path.getmtime(f))
+
+
+def process_files(directory):
+    """Funcion process_files."""
+    files = get_sorted_files(directory)
+    for file in files:
+        print("=" * 50)
+        print(file)
+        folder_name = os.path.basename(os.path.dirname(file))
+        file_name = os.path.basename(file)
+        video = VideoList(
+            folder=folder_name,
+            name=file_name,
+            created_at=datetime.fromtimestamp(os.path.getmtime(file)),
+            opened=False
+        )
+        try:
+            session.add(video)
+            session.commit()
+        except IntegrityError:
+            print("Video already exists")
+            session.rollback()
+
+
+def main():
+    """Funcion main."""
+    directory = "/media/r/RHD00P0_DATA/videos/musica/"
+    process_files(directory)
+
+
+if __name__ == "__main__":
+    main()
